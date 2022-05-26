7215ed19af649fe33223b65dbf36a8ce592d714e
Gabriel Gomes Meira <62515106+Gabriel-Gomes-Meira@users.noreply.github.com>
2022-05-26 20:17:46 -0300

    Base only
    A ideia do script é essa, mas ainda não testei...

---
+++ main.rb
@@ -0,0 +1,26 @@
+require "mongo"
+
+client = Mongo::Client.new([ '127.0.0.1:27017' ],
+                           :database => 'mining_net_development')
+                           
+db = client.database                         
+tasks = db[:tasks_queue]  ### getting "Queued Task" Collection, where will be my, still not completed, tasks. 
+ 
+un_tasks = tasks.find({}).sort(updated_at:-1) ### getting all task's documents sorted by last updated time.
+for t in untasks do
+  f = File.new("script_name.rb","w")
+  f.write(t[:content])
+  f.close 
+  
+  if system(ruby script_name.rb)
+    ##delete from this collection, and insert on tasks_log
+    client[:tasks_log].insert_on(t.exclude(:_id))
+    tasks.find(:_id => t[:_id].delete_one
+    
+  else  
+    ### increment count_erros,
+    tasks.update_one({:_id => t[:_id])}, 
+                            { "$inc" => { :count_erro => 1} } })
+  end
+end
+
