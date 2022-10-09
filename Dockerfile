49790d1726e26166423f273a57cd9a99d921f7d5
Gabriel Gomes Meira <62515106+Gabriel-Gomes-Meira@users.noreply.github.com>
2022-10-09 16:06:18 -0300

    Instalando dependenciais de sistema
    Também executei o main, e deve estar funcionando. Basta passar pro teste de ambiente de produção.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 # syntax=docker/dockerfile:1
 FROM ruby:3.1.2
-RUN apt-get update -qq && apt-get install -y sqlite3
+RUN apt-get update -qq && apt-get install -y sqlite3 wget curl firefox-esr
 WORKDIR /myapp
 COPY Gemfile /myapp/Gemfile
 COPY Gemfile.lock /myapp/Gemfile.lock
+COPY geckodriver /usr/bin
+RUN chmod +x /usr/bin/geckodriver
 RUN bundle install
 RUN gem install sqlite3
 # Add a script to be executed every time the container starts.
--- main.rb
+++ main.rb
@@ -3,55 +3,59 @@ require "sequel"
 
 servicewd = Dir::getwd  ##getting working directory to grant that anyone script will be executed on main work directory's service.
 db = Sequel.connect('sqlite://database/scaveed_development.db')
-                           
-                         
+
+
 tasks = db[:queued_tasks]  ### getting "Queued Task" Table, where will be my, still not completed, tasks.
 
 ### getting the first task (by updated_at)
 while tasks.order(:updated_at).first
-  t = tasks.order(:updated_at).first
-  ## Criar arquivo pronto para logar e executar, no diretório de execução
-  content = ['$log = ""', 'def run', '$log = "==============Iniciando execução=================\n\n"']
-  content = content + t[:content]
-  content.push('$log += "==============Terminando execução=================\n\n"',
-               'return true', 'rescue StandardError => e', '$log += e.full_message',
-               'return false', 'end')
-  Dir::chdir(servicewd)
-  file = File.new(t[:file_name], "w")
-  file.write(content.join("\n"))
-  file.close
-  path_creation = "#{servicewd}/#{t[:file_name]}"
+  sleep 60
+  db.transaction do
+    t = tasks.order(:updated_at).first
+    ## Criar arquivo pronto para logar e executar, no diretório de execução
+    content = ['$log = ""', 'def run', '$log = "==============Iniciando execução=================\n\n"']
+    content += t[:content].split("\n")
+    content.push('$log += "==============Terminando execução=================\n\n"',
+                 'return true', 'rescue StandardError => e', '$log += e.full_message',
+                 'return false', 'end')
+    Dir::chdir(servicewd)
+    file = File.new(t[:file_name], "w")
+    file.write(content.join("\n"))
+    file.close
+    path_creation = "#{servicewd}/#{t[:file_name]}"
 
-  begin
-    require_relative t[:file_name]
-    tasks.where(id: t[:id]).update(state: 1, initialized_at: Time.new)
+    begin
+      require_relative t[:file_name]
+      tasks.where(id: t[:id]).update(state: 1, initialized_at: Time.new)
 
-    if run
+      if run
         ##delete from this table, and insert on tasks_log
         t[:terminated_at] = Time.new
         t[:log]  = $log
         db[:log_tasks].insert(t.except(:id))
         tasks.where(id: t[:id]).delete
-    else
-      ### increment count_erros
-      tasks.where(id: t[:id]).update(log: $log, state: 0, updated_at: Time.new,
+      else
+        ### increment count_erros
+        tasks.where(id: t[:id]).update(log: $log, state: 0, updated_at: Time.new,
+                                       count_erro: t[:count_erro]+1)
+      end
+
+    rescue StandardError => e
+      tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
                                      count_erro: t[:count_erro]+1)
+
+    rescue SyntaxError => e
+      tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
+                                     count_erro: t[:count_erro]+1)
+
+    rescue LoadError => e
+      tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
+                                     count_erro: t[:count_erro]+1)
+
+
     end
-    
-  rescue StandardError => e
-    tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
-                                     count_erro: t[:count_erro]+1)
 
-  rescue SyntaxError => e
-    tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
-                                     count_erro: t[:count_erro]+1)
-
-  rescue LoadError => e
-    tasks.where(id: t[:id]).update(log: e.full_message, state: 0, updated_at: Time.new,
-                                     count_erro: t[:count_erro]+1)
-
+    ## remover arquivo após uso.
+    `rm #{path_creation}`
   end
-
-  ## remover arquivo após uso.
-  `rm #{path_creation}`
 end
