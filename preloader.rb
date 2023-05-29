b25ac84fb1f573b0834b4196efc62d6ef73d4467
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-05-29 10:27:21 -0300

    Created function output_log

--- preloader.rb
+++ preloader.rb
@@ -1,12 +1,16 @@
 
+def output_log(connection, task, log)
+    $log += log    
+    connection[:queued_tasks].where(id: task[:id]).update(updated_at: Time.new, log: $log)
+end
 
 def prepare_enviroment(task)
     Dir::chdir(Dir::getwd)
 
-    $log = "==============Preparando ambiente=================\n\n"    
+    output_log "==============Preparando ambiente=================\n\n"    
     for line in task[:preset_content].split("\n") do
-        $log += `#{line}`
+        output_log `#{line}`
     end
-    $log += "==============Ambiente preparado=================\n\n"
+    output_log "==============Ambiente preparado=================\n\n"
 end
 
